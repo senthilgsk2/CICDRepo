b011c17f5df7eea9159f424473587a8fb09d5ecc
Senthil Kumar G <mgsenthilmgs@gmail.com>
2024-07-06 12:25:16 +0530

    test cicd

--- src/test/java/com/base/BaseClass.java
+++ src/test/java/com/base/BaseClass.java
@@ -10,6 +10,7 @@ public class BaseClass {
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
 		driver.get("https://www.google.com/");
+		driver.get("https://www.google.com/");
 
 	}
 }
